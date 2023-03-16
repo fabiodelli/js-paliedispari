3071a4109e11c769b56853431c0b35988cd824d8
fabiodelli <ntqdel@hotmail.com>
2023-03-16 17:29:57 +0100

    change process

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-const insertWord = prompt("inserisci una parola")
+/* const insertWord = prompt("inserisci una parola") */
 /* let start = insertWord
 let end = insertWord
 let startArray = []
@@ -24,29 +24,31 @@ if(startArray === endArray){
 } */
 
 
-function palindromo(Word){
-let start = Word
-let end = Word
-let startArray = []
-console.log(startArray)
-let endArray = []
-console.log(endArray)
+// Chiedi all'utente di inserire una parola
+let insertWord = prompt("Inserisci una parola:");
 
-for (let i = 0; i < start.length; i++) {
-    const currentLecter = start[i];
-    startArray.push(currentLecter)  
+// Funzione per verificare se la parola è palindroma
+function palindromo(word) {
+  // Rimuovi eventuali spazi e rendi tutto minuscolo
+  word = word.trim().toLowerCase();
+  // Confronta la parola originale con la sua versione invertita
+  return word === word.split("").reverse().join("");
 }
 
-for (let i = end.length-1;i>=0; i--) {
-    const currentLecter = end[i];
-    endArray.push(currentLecter)  
-}
-
-if(startArray == endArray){
-    console.log("palindromo")
-}else{
-    console.log("non palindromo")
-}
+// Verifica se la parola inserita dall'utente è palindroma
+if (palindromo(insertWord)) {
+  console.log(insertWord + " è un palindromo!");
+} else {
+  console.log(insertWord + " non è un palindromo.");
 }
 
 palindromo(insertWord)
+
+/* const endString = endArray.toString()
+console.log(endString) */
+//Pari o dispari
+
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+//Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Dichiariamo chi ha vinto.
