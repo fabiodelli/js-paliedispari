a672b04a621f752bbbb6abbe5d443e85843a542c
fabiodelli <ntqdel@hotmail.com>
2023-03-16 15:52:50 +0100

    add cicle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,15 @@
 
-alert("CIAO")
+const insertWord = prompt("inserisci una parola")
+
+
+
+
+for (let i = 0; i < insertWord.length; i++) {
+    const currentLecter = insertWord[i];
+    console.log(insertWord[i])   
+}
+
+for (let i = insertWord.length; i <= insertWord.length && i>=0; i--) {
+    const currentLecter = insertWord[i];
+    console.log(insertWord[i])   
+}
