9ef46df2bbcc3c5023d9e93f9a614a2abb4f39cb
fabiodelli <ntqdel@hotmail.com>
2023-03-16 16:49:09 +0100

    add function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 
 const insertWord = prompt("inserisci una parola")
-let start = insertWord
+/* let start = insertWord
 let end = insertWord
 let startArray = []
 console.log(startArray)
@@ -21,4 +21,32 @@ if(startArray === endArray){
     console.log("palindromo")
 }else{
     console.log("non palindromo")
+} */
+
+
+function palindromo(Word){
+let start = Word
+let end = Word
+let startArray = []
+console.log(startArray)
+let endArray = []
+console.log(endArray)
+
+for (let i = 0; i < start.length; i++) {
+    const currentLecter = start[i];
+    startArray.push(currentLecter)  
 }
+
+for (let i = end.length-1;i>=0; i--) {
+    const currentLecter = end[i];
+    endArray.push(currentLecter)  
+}
+
+if(startArray == endArray){
+    console.log("palindromo")
+}else{
+    console.log("non palindromo")
+}
+}
+
+palindromo(insertWord)
