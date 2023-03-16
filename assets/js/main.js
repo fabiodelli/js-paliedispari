7416d374db4af6f4dcc22973537eb0f47160cae2
fabiodelli <ntqdel@hotmail.com>
2023-03-16 16:30:08 +0100

    add control

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,24 @@
 
 const insertWord = prompt("inserisci una parola")
+let start = insertWord
+let end = insertWord
+let startArray = []
+console.log(startArray)
+let endArray = []
+console.log(endArray)
 
-
-
-
-for (let i = 0; i < insertWord.length; i++) {
-    const currentLecter = insertWord[i];
-    console.log(insertWord[i])   
+for (let i = 0; i < start.length; i++) {
+    const currentLecter = start[i];
+    startArray.push(currentLecter)  
 }
 
-for (let i = insertWord.length; i <= insertWord.length && i>=0; i--) {
-    const currentLecter = insertWord[i];
-    console.log(insertWord[i])   
+for (let i = end.length-1;i>=0; i--) {
+    const currentLecter = end[i];
+    endArray.push(currentLecter)  
+}
+
+if(startArray === endArray){
+    console.log("palindromo")
+}else{
+    console.log("non palindromo")
 }
